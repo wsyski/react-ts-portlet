3f1eb53869f6fd12cb0d32fd7d8c7f7d7910b658
wsyski <wsyski@gmail.com>
2023-04-16 18:44:32 +0200

    Initial version

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {I18nUtil, LiferayParams, LiferayUtil} from "@arena/lib-portlet-common";
+import {I18nUtil, LIFERAY_PARAMS_DEFAULT, LiferayParams, LiferayUtil} from "@arena/lib-portlet-common";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { I18nextProvider } from 'react-i18next';
@@ -6,13 +6,21 @@ import { I18nextProvider } from 'react-i18next';
 import AppContainer from './AppContainer';
 import { PORTLET_INSTANCE_DEFAULT } from './constants/LiferayParamsConstants';
 
+const isPortal = () => {
+    return process.env.NODE_ENV === 'production';
+}
+
 const index = (liferayParams: LiferayParams) => {
     const liferayParamsWithDefaults = LiferayUtil.setLiferayParamsDefaults(liferayParams, PORTLET_INSTANCE_DEFAULT);
     const i18nInstance = I18nUtil.init(liferayParamsWithDefaults.contextPath);
     const markup = React.createElement(I18nextProvider, { i18n: i18nInstance }, React.createElement(AppContainer, {
         liferayParams: liferayParamsWithDefaults,
     }));
-    ReactDOM.render(React.createElement(React.Fragment, null, markup), document.getElementById(liferayParamsWithDefaults.portletElementId));
+    ReactDOM.render(isPortal() ? React.createElement(React.Fragment, null, markup)  : React.createElement(React.StrictMode, null, markup), document.getElementById(liferayParamsWithDefaults.portletElementId));
 };
+
+if (!isPortal()) {
+    index(LIFERAY_PARAMS_DEFAULT);
+}
 
 export default index;
