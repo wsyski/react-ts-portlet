82639d9628a54a9a4cecb2f05a9975168c5e12f8
wsyski <wsyski@gmail.com>
2023-04-16 13:33:45 +0200

    Initial version

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,8 @@
 'use strict';
 
 var gulp = require('gulp');
-var sass = require('gulp-dart-sass');
 
-var buildUtils = require('./scripts/build-utils');
-
-gulp.task('scss', () => {
-	return gulp
-		.src(['./src/css/**/*.scss'])
-		.pipe(
-			sass({includePaths: 'node_modules/@clayui/css/src/scss'}).on(
-				'error',
-				sass.logError
-			)
-		)
-		.pipe(gulp.dest('./src/css'));
-});
-
-gulp.task('scss:watch', () => {
-	gulp.watch('./src/css/**/*.scss', ['sass']);
-});
+var buildUtils = require('../../../../scripts/build-utils');
 
 gulp.task('publish', (cb) => {
 	buildUtils.publish();
@@ -27,9 +10,5 @@ gulp.task('publish', (cb) => {
 });
 gulp.task('deploy', (cb) => {
 	buildUtils.deploy();
-	cb();
-});
-gulp.task('clean', (cb) => {
-	buildUtils.clean(__dirname);
 	cb();
 });
